b3118fc04495a3f346993595835ec5e8f07e2706
Igor Popov <igor@igorpopov.io>
2014-05-04 16:09:22 +0300

    Simplify installation
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- setup.sh
+++ setup.sh
@@ -6,6 +6,5 @@ cp configuration-files/ROOT.xml artifactory-3.2.0/tomcat/conf/Catalina/localhost
 cp configuration-files/server.xml artifactory-3.2.0/tomcat/conf
 cp configuration-files/index.html artifactory-3.2.0/tomcat/webapps/ROOT
 mv artifactory-3.2.0/tomcat/conf/Catalina/localhost/artifactory.{xml,xml.backup}
-mv artifactory-3.2.0/webapps/{artifactory,ROOT}.war
 artifactory-3.2.0/bin/artifactory.sh
 
