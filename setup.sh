c3df6cad6a60466954de802036ee05faf98e4ae1
Igor Popov <igor@igorpopov.io>
2014-05-04 14:40:17 +0300

    Use port 80 and redirect to webapp
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 rm -rf artifactory-3.2.0
 unzip artifactory-3.2.0.zip
 cp configuration-files/ROOT.xml artifactory-3.2.0/tomcat/conf/Catalina/localhost
+cp configuration-files/server.xml artifactory-3.2.0/tomcat/conf
+cp configuration-files/index.html artifactory-3.2.0/tomcat/webapps/ROOT
 mv artifactory-3.2.0/tomcat/conf/Catalina/localhost/artifactory.{xml,xml.backup}
 mv artifactory-3.2.0/webapps/{artifactory,ROOT}.war
 artifactory-3.2.0/bin/artifactory.sh
